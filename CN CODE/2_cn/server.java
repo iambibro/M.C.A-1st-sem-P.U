a49dc020a0f008721779a97ff882d3f63e957996
Bibrata Barman <33283305+iambibro@users.noreply.github.com>
2021-01-18 20:44:23 +0530

    Create server.java

---
+++ CN CODE/2_cn/server.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.io.*;
+public class server{
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+        System.out.println("Client connected");
+
+        InputStreamReader in =new InputStreamReader(s.getInputStream());
+        BufferedReader bf= new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Client:"+ str);
+
+        PrintWriter pr =new PrintWriter(s.getOutputStream());
+        pr.println("  hi client...");
+        pr.flush();
+    }
+}
